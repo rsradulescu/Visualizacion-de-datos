1465a58e68d36edd4daf8ba4bae8f15bad8a9f29
rsradulescu <rsradulescu@gmail.com>
2021-11-06 17:28:31 -0300

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Master en Big Data y Data Science - VIU</h1>`;
+
+// ejercicio de Javascript
+var visualization = d3plus.viz()
+  .container("#viz1")
+  .data([{"year": 1991, "name":"alpha", "value": 15},
+    {"year": 1992, "name":"alpha", "value": 20},])
+  .type("bar")
+  .id("name")
+  .x("year")
+  .y("value")
+  .draw()
